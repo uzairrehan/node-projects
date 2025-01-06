f7c73c4b90627cacaaff2892878979222dc3851c
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-06 09:30:02 +0500

    file creation app completed

--- cli-file-creation-app/index.js
+++ cli-file-creation-app/index.js
@@ -1,13 +1,26 @@
 import fs from "fs";
 import readline from "readline";
 
-
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+function show() {
+  rl.question(
+    "Enter File name with extension you want to create : ",
+    handleFileCreation
+  );
+}
 
+function handleFileCreation(nameOutput) {
+  rl.question("Enter file data here : ", (dataOutput) => {
+    fs.writeFile(nameOutput, dataOutput, "utf-8", (err) => {
+      if (err) console.log(err);
+    });
+    console.log("File Created!");
+    rl.close();
+  });
+}
 
-console.log(fs);
+show();
